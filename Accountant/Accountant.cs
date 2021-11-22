6157a811bffefcb63f96c9213d6a27d8ab3b86d7
Ottermandias <nickroederspam+git1@gmail.com>
2021-11-22 14:02:41 +0100

    Fix /acct opening config instead of timers.

--- Accountant/Accountant.cs
+++ Accountant/Accountant.cs
@@ -16,9 +16,9 @@ public class Accountant : IDalamudPlugin
 
     public static string Version = "";
 
-    public static   AccountantConfiguration Config    = null!;
-    public static   IGameData               GameData  = null!;
-    public static   IAddonWatcher           Watcher   = null!;
+    public static   AccountantConfiguration Config   = null!;
+    public static   IGameData               GameData = null!;
+    public static   IAddonWatcher           Watcher  = null!;
     public readonly TimerManager            Timers;
     public readonly TimerWindow             TimerWindow;
     public readonly ConfigWindow            ConfigWindow;
@@ -29,8 +29,8 @@ public class Accountant : IDalamudPlugin
         Version = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "";
         Config  = AccountantConfiguration.Load();
 
-        Watcher   = AddonWatcherFactory.Create(Dalamud.GameGui, Dalamud.SigScanner);
-        GameData  = GameDataFactory.Create(Dalamud.GameGui, Dalamud.ClientState, Dalamud.GameData);
+        Watcher  = AddonWatcherFactory.Create(Dalamud.GameGui, Dalamud.SigScanner);
+        GameData = GameDataFactory.Create(Dalamud.GameGui, Dalamud.ClientState, Dalamud.GameData);
 
         Timers       = new TimerManager();
         TimerWindow  = new TimerWindow(Timers);
@@ -61,8 +61,11 @@ public class Accountant : IDalamudPlugin
         Config.Save();
     }
 
-    private void OnAcct(string command, string _)
-        => ConfigWindow.Toggle();
+    private static void OnAcct(string command, string _)
+    {
+        Config.WindowVisible = !Config.WindowVisible;
+        Config.Save();
+    }
 
     public void Dispose()
     {
