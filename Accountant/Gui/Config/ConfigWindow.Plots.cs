f11f25a326313a0c37b6abcc4c4b7e2591a132e8
Ottermandias <nickroederspam+git1@gmail.com>
2024-09-16 18:41:07 +0200

    Small fixes.

--- Accountant/Gui/Config/ConfigWindow.Plots.cs
+++ Accountant/Gui/Config/ConfigWindow.Plots.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Linq;
 using System.Numerics;
 using Accountant.Classes;
 using Accountant.Enums;
 using Accountant.Gui.Helper;
-using Accountant.Gui.Timer;
 using Accountant.Manager;
 using Dalamud.Interface;
 using Dalamud.Interface.Components;
@@ -33,9 +30,10 @@ public partial class ConfigWindow
         DrawDemolitionData();
     }
 
+    private ulong    _newPlotInfo = new PlotInfo(InternalHousingZone.Mist, 1, 1, 0).Value;
     private PlotInfo _selectedPlot;
     private string   _newPlayerName = string.Empty;
-    private ushort   _newWorldId    = 0;
+    private ushort   _newWorldId;
 
     private void DrawDemolitionData()
     {
@@ -215,11 +213,11 @@ public partial class ConfigWindow
 
         ImGui.SameLine();
         var currentPlot = _demoManager.CurrentPlot;
-        tt = (currentPlot.Valid(), _demoManager.CanAddPlot(newPlot)) switch
+        tt = (currentPlot.Valid(), _demoManager.CanAddPlot(currentPlot)) switch
         {
-            (true, true)  => $"Add the plot {currentPlot} on {Accountant.GameData.GetWorldName(currentPlot.ServerId)}.",
+            (true, true)  => $"Add the plot {currentPlot.ToName()} on {Accountant.GameData.GetWorldName(currentPlot.ServerId)}.",
             (false, _)    => "You are not on an identifiable plot.",
-            (true, false) => $"The plot {{currentPlot}} on {Accountant.GameData.GetWorldName(currentPlot.ServerId)} was already added",
+            (true, false) => $"The plot {currentPlot.ToName()} on {Accountant.GameData.GetWorldName(currentPlot.ServerId)} was already added",
         };
         using (ImRaii.Disabled(tt[0] != 'A'))
         {
@@ -250,89 +248,5 @@ public partial class ConfigWindow
 
         if (ImGui.IsItemHovered(ImGuiHoveredFlags.AllowWhenDisabled))
             ImGui.SetTooltip(tt);
-    }
-}
-
-public partial class ConfigWindow
-{
-    private ulong  _newPlotInfo = new PlotInfo(InternalHousingZone.Mist, 1, 1, 0).Value;
-    private string _newPlotName = string.Empty;
-
-    private void DrawPlotNamesTab()
-    {
-        if (!ImGui.BeginTabItem("Plot Names##AccountantTabs"))
-            return;
-
-        using var raii = ImGuiRaii.DeferredEnd(ImGui.EndTabItem);
-
-        if (!ImGui.BeginChild("##PlotNamesTab"))
-            return;
-
-        raii.Push(ImGui.EndChild);
-
-        using var ids = ImGuiRaii.PushId("PlotNames");
-        if (!ImGui.BeginTable("", 6, ImGuiTableFlags.None, ImGui.GetContentRegionAvail()))
-            return;
-
-        raii.Push(ImGui.EndTable);
-
-        ImGui.TableSetupColumn("", ImGuiTableColumnFlags.WidthFixed, ImGui.GetStyle().FrameBorderSize);
-        SetupPlotHeaders();
-        ImGui.TableSetupColumn("Name", ImGuiTableColumnFlags.WidthStretch);
-        ImGui.TableHeadersRow();
-        ImGui.TableSetupScrollFreeze(0, 1);
-
-        ulong?  change  = null;
-        string? newName = null;
-        foreach (var (value, name) in Accountant.Config.PlotNames)
-        {
-            var tmp = name;
-            ImGui.TableNextColumn();
-            using var font = ImGuiRaii.PushFont(UiBuilder.IconFont);
-            if (ImGui.Button($"{FontAwesomeIcon.Trash.ToIconChar()}##{value}"))
-                change = value;
-            font.Pop();
-            DrawPlotRow(PlotInfo.FromValue(value));
-            ImGui.TableNextColumn();
-            ImGui.SetNextItemWidth(-1);
-            if (!ImGui.InputText($"##{value}", ref tmp, 32, ImGuiInputTextFlags.EnterReturnsTrue) || tmp == name)
-                continue;
-
-            change  = value;
-            newName = tmp;
-        }
-
-        var newPlot = PlotInfo.FromValue(_newPlotInfo);
-        if (newPlot.ServerId == 0 && Dalamud.ClientState.LocalPlayer != null)
-        {
-            newPlot      = new PlotInfo(newPlot.Zone, newPlot.Ward, newPlot.Plot, (ushort)Dalamud.ClientState.LocalPlayer.CurrentWorld.Id);
-            _newPlotInfo = newPlot.Value;
-        }
-
-        ImGui.TableNextRow();
-        ImGui.TableNextColumn();
-        using var _ = ImGuiRaii.PushFont(UiBuilder.IconFont);
-        if (ImGui.Button(FontAwesomeIcon.Plus.ToIconString()) && newPlot.ServerId != 0)
-        {
-            change  = _newPlotInfo;
-            newName = _newPlotName;
-        }
-
-        _.Pop();
-
-        DrawPlotInfoInput(111, ref _newPlotInfo);
-        ImGui.TableNextColumn();
-        ImGui.SetNextItemWidth(-1);
-        ImGui.InputTextWithHint("##name", "New Plot Name...", ref _newPlotName, 32);
-
-        if (change == null)
-            return;
-
-        if (newName != null && newName.Any())
-            Accountant.Config.PlotNames[change.Value] = newName;
-        else
-            Accountant.Config.PlotNames.Remove(change.Value);
-        Accountant.Config.Save();
-        _timerWindow.ResetCache(typeof(TimerWindow.CropCache));
     }
 }
